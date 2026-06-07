9dab102975f54d699a620e2ffc11062485db6b99
niro3-1 <niro3-1@walla.co.il>
2026-06-07 13:56:33 +0300

    Update Sidebar component with responsive width

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div style={{ width: 'min(240px, 100vw)' }}>
+      <h2>Sidebar</h2>
+      <p>This is the sidebar component with a responsive width.</p>
+    </div>
+  );
+};
+
+export default Sidebar;
